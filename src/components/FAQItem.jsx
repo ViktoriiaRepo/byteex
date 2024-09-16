7565992fb19cf6898b6d0a18f2012740283fab1c
ViktoriiaRepo <samarityankav@gmail.com>
2024-09-16 23:52:48 +0300

    Added some  animations

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -1,25 +1,39 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const FAQItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.style.maxHeight = isOpen
+        ? `${contentRef.current.scrollHeight}px`
+        : '0';
+    }
+  }, [isOpen]);
 
   return (
-    <div className='border-b border-border-color  '>
-      <div className='flex justify-between  py-4'>
-        <span className='text-accent-color text-[18px] leading-[133%] tracking-[0.04em]'>
+    <div className='border-b border-gray-300'>
+      <div className='flex justify-between py-4'>
+        <span className='text-accent-color text-lg leading-tight tracking-wide'>
           {question}
         </span>
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className='text-accent-color text-[44px]  leading-[0] tracking-[0.04em] hover:text-gray-700 focus:outline-none  '
+          className='text-accent-color text-2xl hover:text-gray-700 focus:outline-none'
         >
           {isOpen ? '-' : '+'}
         </button>
       </div>
-      {isOpen && (
-        <p className='py-4 text-[14px] tracking-[0.03em] '>{answer}</p>
-      )}
+      <div
+        ref={contentRef}
+        className={`overflow-hidden transition-max-height duration-300 ease-out ${
+          isOpen ? 'max-h-screen' : 'max-h-0'
+        }`}
+      >
+        <p className='py-4 text-sm tracking-wide'>{answer}</p>
+      </div>
     </div>
   );
 };
--- src/sections/HeroSection/HeroSection.jsx
+++ src/sections/HeroSection/HeroSection.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { motion } from 'framer-motion';
+
 import FreeShippingBanner from '../../components/FreeShippingBanner';
 import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
 import CustomizeOutfitButton from '../../components/CustomizeOutfitButton';
@@ -13,6 +15,7 @@ import { fetchProducts } from '../../redux/productsSlice';
 const HeroSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [slides, setSlides] = useState([]);
+  const [reviewKey, setReviewKey] = useState(0);
 
   const { products, status, error } = useSelector((state) => state.products);
   const dispatch = useDispatch();
@@ -38,6 +41,7 @@ const HeroSection = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+      setReviewKey((prevKey) => prevKey + 1);
     }, 3000);
 
     return () => clearInterval(interval);
@@ -62,15 +66,28 @@ const HeroSection = () => {
       >
         <div className='flex flex-col md:desktop:flex-row md:desktop:gap-[94px]'>
           <div className='flex flex-col items-center md:desktop:items-start md:desktop:w-[calc(100%-725px-94px)]'>
-            <div className='flex justify-center md:desktop:justify-start items-center mt-[14px] md:desktop:mt-[33px]'>
-              <Logo className='w-[200px] h-[35px]' />
-            </div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              transition={{ duration: 1 }}
+            >
+              <div className='flex justify-center md:desktop:justify-start items-center mt-[14px] md:desktop:mt-[33px]'>
+                <Logo className='w-[200px] h-[35px]' />
+              </div>
+            </motion.div>
 
-            <h2 className='mt-[14px] md:desktop:mt-[62px] md:desktop:text-[38px] text-h2custom text-accent-color text-center md:desktop:text-left'>
+            <motion.h2
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 50 }}
+              transition={{ duration: 1 }}
+              className='mt-[14px] md:desktop:mt-[62px] md:desktop:text-[38px] text-h2custom text-accent-color text-center md:desktop:text-left'
+            >
               Don’t apologize for being comfortable.
-            </h2>
+            </motion.h2>
 
-            <div className='hidden md:desktop:flex  flex-col gap-4 mt-[20px]  md:desktop:mt-[59px] md:desktop:w-full md:desktop:max-w-[414px]'>
+            <div className='hidden md:desktop:flex flex-col gap-4 mt-[20px] md:desktop:mt-[59px] md:desktop:w-full md:desktop:max-w-[414px]'>
               <HeroList />
               <CustomizeOutfitButton />
               <Reviews currentReview={currentReview} />
@@ -81,10 +98,18 @@ const HeroSection = () => {
             <SliderHero slides={slides} currentIndex={currentIndex} />
           </div>
 
-          <div className='flex flex-col gap-4 md:desktop:hidden  mt-[20px]'>
+          <div className='flex flex-col gap-4 md:desktop:hidden mt-[20px]'>
             <HeroList />
             <CustomizeOutfitButton />
-            <Reviews currentReview={currentReview} />
+            <motion.div
+              key={reviewKey}
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              transition={{ duration: 1 }}
+            >
+              <Reviews currentReview={currentReview} />
+            </motion.div>
           </div>
         </div>
       </div>
